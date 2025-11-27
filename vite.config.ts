6341bcbd279961672b559bf1789652e59ee41dc5
Abdirahman Afrah <abdirahmanafarh67@gmail.com>
2025-11-27 08:46:25 -0800

    Fix Vercel build configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // REQUIRED for Vercel
+  base: "/", // REQUIRED for Vercel
   build: {
     outDir: "dist", // Vercel needs this
   },
